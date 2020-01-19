290656437f46e8764a3d5b24853d15da80fbba1c
kylegreenf <kylegreenf@gmail.com>
2020-01-18 17:53:42 -0800

    Text to speech works

--- app/src/main/java/com/example/a4sightmyopiaexamination/MainActivity.java
+++ app/src/main/java/com/example/a4sightmyopiaexamination/MainActivity.java
@@ -1,14 +1,67 @@
 package com.example.a4sightmyopiaexamination;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView txtResult;
+    private TextView letterTesting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtResult = (TextView) findViewById(R.id.txtResult);
+        letterTesting = (TextView) findViewById(R.id.letterTesting);
+        letterTesting.setText(generateRandomLetter());
+    }
+
+    public void getSpeechInput(View view) {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, 10);
+        } else {
+            Toast.makeText(this, "Your device does not support speech input", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case 10:
+                if (resultCode == RESULT_OK && data != null) {
+                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    if (result.get(0) != null) {
+                        txtResult.setText(result.get(0));
+                        letterTesting.setText(generateRandomLetter());
+                    }
+                }
+                break;
+        }
+    }
+
+    public String generateRandomLetter() {
+        char letter;
+        Random r = new Random();
+        int l = r.nextInt(26) + 65;
+        letter = (char) l;
+        return letter + "";
     }
 }
