0cbd3cfd676ed5353d831253afc716561658eb5f
kylegreenf <kylegreenf@gmail.com>
2020-01-27 14:27:22 -0800

    Each letter is phonetically represented, EX: B = B, Bee, T = T, Tea, Tee, etc.

--- app/src/main/java/com/example/a4sightmyopiaexamination/MainActivity.java
+++ app/src/main/java/com/example/a4sightmyopiaexamination/MainActivity.java
@@ -88,10 +88,12 @@ public class MainActivity extends AppCompatActivity {
 
     private void checkCorrectGuess(String result, String correctAnswer) {
         String guess = result;
-        if (guess.toLowerCase().equals(correctAnswer.toLowerCase())) {
+        totalQuestions++;
+        if (isPossibleAnswer(guess.toLowerCase(), correctAnswer.toLowerCase())) {
             correctGuess.start(); // Play a noise
             Toast.makeText(this, "Correct input!", Toast.LENGTH_SHORT).show();
         } else {
+            incorrectCount++;
             if (incorrectCount == 3) {
                 // Open diagnosis/analyze
             }
@@ -101,5 +103,144 @@ public class MainActivity extends AppCompatActivity {
         if (totalQuestions == 20) {
             // Open diagonsis/analyze
         }
+    }
+
+    private boolean isPossibleAnswer(String guess, String correctAnswer) {
+
+        switch (correctAnswer) {
+            case "a":
+                if (guess.equals("a") || guess.equals("hey") || guess.equals("aye")) {
+                    return true;
+                }
+                break;
+            case "b":
+                if (guess.equals("b") || guess.equals("bee")) {
+                    return true;
+                }
+                break;
+            case "c":
+                if (guess.equals("c") || guess.equals("see")) {
+                    return true;
+                }
+                break;
+            case "d":
+                if (guess.equals("d") || guess.equals("dee") || guess.equals("the")) {
+                    return true;
+                }
+                break;
+            case "e":
+                if (guess.equals("e") || guess.equals("eek") || guess.equals("aye")) {
+                    return true;
+                }
+                break;
+            case "f":
+                if (guess.equals("f")) {
+                    return true;
+                }
+                break;
+            case "g":
+                if (guess.equals("g") || guess.equals("gee")) {
+                    return true;
+                }
+                break;
+            case "h":
+                if (guess.equals("h")) {
+                    return true;
+                }
+                break;
+            case "i":
+                if (guess.equals("i") || guess.equals("eye")) {
+                    return true;
+                }
+                break;
+            case "j":
+                if (guess.equals("j") || guess.equals("jay")) {
+                    return true;
+                }
+                break;
+            case "k":
+                if (guess.equals("k") || guess.equals("ok") || guess.equals("kay")) {
+                    return true;
+                }
+                break;
+            case "l":
+                if (guess.equals("l") || guess.equals("elle")) {
+                    return true;
+                }
+                break;
+            case "m":
+                if (guess.equals("m") || guess.equals("um") || guess.equals("ehm")) {
+                    return true;
+                }
+                break;
+            case "n":
+                if (guess.equals("n") || guess.equals("and n") || guess.equals("end")) {
+                    return true;
+                }
+                break;
+            case "o":
+                if (guess.equals("o") || guess.equals("oh")) {
+                    return true;
+                }
+                break;
+            case "p":
+                if (guess.equals("p") || guess.equals("pee")) {
+                    return true;
+                }
+                break;
+            case "q":
+                if (guess.equals("q") || guess.equals("que")) {
+                    return true;
+                }
+                break;
+            case "r":
+                if (guess.equals("r") || guess.equals("our") || guess.equals("are")) {
+                    return true;
+                }
+                break;
+            case "s":
+                if (guess.equals("s") || guess.equals("essay") || guess.equals("ess")) {
+                    return true;
+                }
+                break;
+            case "t":
+                if (guess.equals("t") || guess.equals("tea") || guess.equals("tee")) {
+                    return true;
+                }
+                break;
+            case "u":
+                if (guess.equals("u") || guess.equals("you")) {
+                    return true;
+                }
+                break;
+            case "v":
+                if (guess.equals("v") || guess.equals("v")) {
+                    return true;
+                }
+                break;
+            case "w":
+                if (guess.equals("w") || guess.equals("double you")) {
+                    return true;
+                }
+                break;
+            case "x":
+                if (guess.equals("x") || guess.equals("ex")) {
+                    return true;
+                }
+                break;
+            case "y":
+                if (guess.equals("y") || guess.equals("why")) {
+                    return true;
+                }
+                break;
+            case "z":
+                if (guess.equals("z") || guess.equals("zee")) {
+                    return true;
+                }
+                break;
+
+        }
+
+        return false;
     }
 }
