e4b17357ce82253ba167967585618edf6451aee4
greenfe2 <greenfe2.wwu.edu>
2020-02-07 09:46:46 -0800

    Set up cases for proposed size

--- app/src/main/java/com/example/a4sightmyopiaexamination/AfterTestAnalysis.java
+++ app/src/main/java/com/example/a4sightmyopiaexamination/AfterTestAnalysis.java
@@ -28,4 +28,30 @@ public class AfterTestAnalysis extends AppCompatActivity {
         String result = Integer.toString(fontSize);
         score.setText("You can read letters at " + result + " pt font size. Right now that means nothing!");
     }
+
+    private void findScore(int fontSize) {
+        switch (fontSize) { //Different font sizes assign to different scores
+            case 160: //Uncalculatable 0%
+                break;
+            case 105: //2.0-2.5ph 10%
+                break;
+            case 80: //1.75-2.0sph 20%
+                break;
+            case 66: // 1.5sph 30%
+                break;
+            case 60: //1.0-1.25sph 40%
+                break;
+            case 49: //.5sph 50%
+                break;
+            case 45: //.5sph 60%
+                break;
+            case 41: //.5sph 80%
+                break;
+            case 33: //0-.25sph 90%
+                break;
+            default: //0 - if they get 33pt font correct
+                break;
+
+        }
+    }
 }
